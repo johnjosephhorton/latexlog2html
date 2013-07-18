42017e7b11495e1cc385beabdb090d3607d1660c
John Horton <john.joseph.horton@gmail.com>
2013-07-17 17:02:46 -0700

    MVP

--- latexlog2html.py
+++ latexlog2html.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python
+import argparse 
 import os 
 import re 
-
 
 def create_anchor(notice_type, n): 
     return "</pre><a name='%s%s'><a href='#top'><h2>Back to top</h2></a><pre>" % (
@@ -11,7 +12,6 @@ def parse_latex_log(log_file_name):
     error_re = r"""!.*"""
     warnings, errors, body = [], [], []
     log_file = open(log_file_name, "r")   
-    body = []
     for line in log_file:
         if re.search(warning_re, line): 
             warnings.append(line)
@@ -24,35 +24,68 @@ def parse_latex_log(log_file_name):
         body.append(line)
     return warnings, errors, body
 
+
 def create_ordered_list(heading, tagname, item_list):
+    """ """
     list_items = []
     for index, item in enumerate(item_list):
         list_items.append("<li><a href='#%s%s'>%s</a></li>" % (tagname, index + 1, item))
     return """<h1>%s</h1>""" % heading  + """<ol>%s</ol>""" % ''.join(list_items)
 
 
-def convert_log(log_file_name, header_template, css_hotlink):
+def convert_log(log_file_name, header_template):
     """ 
     Parses a latex log file and returns HTML. 
     """
     warnings, errors, body = parse_latex_log(log_file_name)
     new_log = []
-    new_log.append(header_template % css_hotlink)
+    new_log.append(header_template)
     new_log.append(create_ordered_list("Errors", "error", errors))
     new_log.append(create_ordered_list("Warning", "warn", warnings))
     new_log += ["<h1>Full Log</h1><pre>"] + body + ['</pre></body></html>']
     return new_log 
 
 
-    latex_log = os.path.join(output_dir, "writeup", "%s.log" % topic)
-    html_latex_log = l2h.convert_log(latex_log, 
-                                    templates.LATEX_LOG_FILE_HEADER,
-                                    settings.CSS_HOTLINK)
-    # write the log file
-    f = open(os.path.join(output_dir, settings.LATEX_HTML_FILE_NAME), "w")
-    f.writelines(html_latex_log)
-    f.close() 
+HTML_HEADER =  """<html><head> 
+ <link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css">
+</head><body> 
+"""
+
+def main():
+    
+    parser = argparse.ArgumentParser(description='Process a LaTeX log file')
+    parser.add_argument("-f", "--file", help = "Log file to parse")
+    parser.add_argument("-x", "--browser", help = "Browser to use to open file")
+    args = parser.parse_args()
+
+
+    g = open("sample.html", "w")
+    html_output = convert_log(args.file, HTML_HEADER)
+        #w, e, b = parse_latex_log(args.file)
+        #print(w, e, b) 
+        # f = open(args.file, "r")
+    g.write(''.join(html_output))
+    g.close()
+    os.system(args.browser + " sample.html")   
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+    # latex_log = os.path.join(output_dir, "writeup", "%s.log" % topic)
+    # html_latex_log = l2h.convert_log(latex_log, 
+    #                                 templates.LATEX_LOG_FILE_HEADER,
+    #                                 settings.CSS_HOTLINK)
+    # # write the log file
+    # f = open(os.path.join(output_dir, settings.LATEX_HTML_FILE_NAME), "w")
+    # f.writelines(html_latex_log)
+    # f.close() 
       
-    report_location = os.path.join(output_dir, settings.EXEC_REPORT)
-    os.system("%s %s" % (settings.BROWSER, report_location))
-    return True    
+    # report_location = os.path.join(output_dir, settings.EXEC_REPORT)
+    # os.system("%s %s" % (settings.BROWSER, report_location))
+    # return True    
